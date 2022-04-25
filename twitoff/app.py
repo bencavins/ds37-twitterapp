67cadf66087b4a8ccd5399f1cf367d6b44cc755a
Ben Cavins <bencavins@gmail.com>
2022-04-25 13:19:55 -0400

    add simple base.html

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+import re
+from flask import Flask, render_template
 
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
+    app_title = "Twitoff DS37!!!!!!"
 
 
-@app.route("/")
-def hello_world():
-    return "<p>hello world!</p>"
+    @app.route("/")
+    def root():
+        return render_template('base.html', title='Home')
+
+    @app.route("/test")
+    def test():
+        return f"<p>This is a page for {app_title}</p>"
+    
+    return app
