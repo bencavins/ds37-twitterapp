a0d6c1794c184a58dbe01490096c3a6a3d982713
Ben Cavins <bencavins@gmail.com>
2022-04-27 13:38:09 -0400

    add predictions

--- twitoff/app.py
+++ twitoff/app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from turtle import title
+from flask import Flask, render_template, request
 from .twitter import add_or_update_user
 from .models import DB, User, Tweet
+from .predict import predict_user
 
 
 def create_app():
@@ -56,6 +58,45 @@ def create_app():
         return render_template(
             'base.html', 
             title="All users have been updated to include their latest tweets."
+        )
+    
+    @app.route('/user', methods=['POST'])
+    @app.route('/user/<user_name>', methods=['GET'])
+    def user(user_name=None, message=''):
+        try:
+            if request.method == 'POST':
+                user_name = request.values['user_name']
+                add_or_update_user(user_name)
+                message = f'User {user_name} was added succesfully'
+            tweets = User.query.filter(User.username == user_name).one().tweets
+        except Exception as e:
+            message = f'Error occurred: {e}'
+            tweets = []
+        
+        return render_template(
+            'user.html', 
+            title=user_name, 
+            tweets=tweets, 
+            message=message
+        )
+    
+    @app.route('/compare', methods=['POST'])
+    def compare():
+        user0 = request.values['user0']
+        user1 = request.values['user1']
+        hypo_tweet = request.values['tweet_text']
+
+        if user0 == user1:
+            message = 'Cannot compare a user to themselves'
+        else:
+            prediction = predict_user(user0, user1, hypo_tweet)
+            predicted_user = user0 if prediction == 0.0 else user1
+            message = f'"{hypo_tweet}" is more likely said by {predicted_user}'
+
+        return render_template(
+            'prediction.html',
+            title='Prediction',
+            message=message
         )
     
     return app
